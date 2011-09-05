ba075d96e9134c311828eb4857866e46a28f140c
Saad Shakhshir <shakhshir@gmail.com>
2011-09-06 00:50:26 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homepinions::Application.config.secret_token = 'f5019e6c21ac3207632d60a2dc730eed8a03a976e41aa06efd586c6887b33546520b0a4f502247b564da9d5429f0d2ecae3a8b481d5287bdfdc6aff012d9ebe4'
